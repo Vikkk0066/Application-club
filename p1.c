d759f1b3b86e0759a51b469949efccc0d7585054
Vikkk0066 <114860633+Vikkk0066@users.noreply.github.com>
2023-01-01 23:11:47 +0530

    Delete p1.c

--- p1.c
+++
@@ -1,6 +0,0 @@
-#include<stdio.h>
-void main()
-{
-    printf("Myself Vikas Kumar Singh.i am pursuing Master Of Computer Application \n from Motilal Nehru National Institute Of Technology.\n");
-    printf("Now i am at first year and living at Madan Mohan Malaviya Hostel");
-}
